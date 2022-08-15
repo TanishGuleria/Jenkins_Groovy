0a173351294d1493c25deeca6642c8de05164fc0
Tanish Guleria <tanish.guleria@sophos.com>
2022-08-16 03:51:54 +0530

    added shared lib

--- vars/lib.groovy
+++ vars/lib.groovy
@@ -3,17 +3,22 @@ def call(body)
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body()
     pipeline{
-    agent any
-    stages {
-        stage ('build'){
-            steps {
-                sh 'echo "shared lib "'
-                sh '''
-                    echo "multi sahred lib"
+        agent any
+        options{
+            buildDiscarder(logRotator(numToKeepStr: '2'))
+            timeout(time: 1, unit: 'HOURS')
+            disableConcurrentBuilds()
+        }
+        stages {
+            stage ('build'){
+                steps {
+                    sh 'echo "shared lib "'
+                    sh '''
+                        echo "multi sahred lib"
                     
-                '''
+                    '''
+                }
             }
         }
     }
-}
 }
